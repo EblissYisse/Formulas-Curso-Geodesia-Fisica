43f4a8dc7e04e5d4a90cf3f9abfb7dcd178c6b6b
Ebliss Yissela Segovia Venales <segoviavenales@hotmail.com>
2022-06-20 21:28:07 -0500

    2

--- Segundo Parcial/Formulas.py
+++ Segundo Parcial/Formulas.py
@@ -3,9 +3,14 @@
 #si el ejercicio te indica una profundidad, pasarla a metros 
 # f = -Km / (r - profundidad en metror) ^2 
 
-#Fuerza de gravedad
+#Fuerza de gravedad en la luna
 
-# g = (-Km / r^2) + w^2 rluna
+# g = - ((K(mluna) /  (rluna)^2 ) + w^2 rluna)
+
+#volumenTierra = 4/3pi^3R^3
+
+#rluna = rt / RAIZ 3 DE 49 = 6370000 / 3.65930571 = 1740767.376
+#rluna = 1740767.376
 
 
  
